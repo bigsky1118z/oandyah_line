d3c6906fc94fd2e8b3b3f467584de6a985658aef
bigsky1118z <bigsky1118z@gmail.com>
2024-03-28 16:38:56 +0900

    type

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -11,7 +11,6 @@
                 <li><a href="/{{ $user->user_name }}/{{ $app->app_name }}" target="_blank" rel="noopener noreferrer">{{ $app->display_name ?? $app->app_name }}</a></li>
             @endforeach
         </ul>
-        {{ $route }}
     </x-slot>
     <x-slot name="footer">
     </x-slot>
