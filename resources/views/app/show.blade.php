77d8f9a32c40bbd6d96886c74cd74ee05e31451e
bigsky1118z <bigsky1118z@gmail.com>
2024-03-28 16:59:47 +0900

    show

--- resources/views/app/show.blade.php
+++ resources/views/app/show.blade.php
@@ -6,6 +6,10 @@
     </x-slot>
     <x-slot name="main">
         <h2>{{ $app->diplay_name ?? $app->app_name }}</h2>
+        <form action="/{{ $user->user_name }}/{{ $app->app_name }}/webhook" method="post">
+            @csrf
+            <button type="submit">post</button>
+        </form>
     </x-slot>
     <x-slot name="footer">
     </x-slot>
