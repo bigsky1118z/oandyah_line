cdcb8f5797bf5d520f927fb3947714d322008add
bigsky1118z <bigsky1118z@gmail.com>
2024-03-28 10:16:21 +0900

    redirect

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+Route::get('redirect', function () {
+    return view('redirect');
+});
+
 
 Route::prefix("dashboard")->group(function(){
     return view('dashboard');
