df56e5aa0258404359ee5e1ff199f7f58cd1a2cf
bigsky1118z <bigsky1118z@gmail.com>
2024-03-28 16:10:19 +0900

    appc

--- app/Http/Controllers/App/AppController.php
+++ app/Http/Controllers/App/AppController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class AppController extends Controller
 {
-    //
+    public function index($user_name)
+    {
+        $user   =   auth()->user();
+        if($user->user_name == $user_name){
+            $data   =   array(
+                "users"  =>  $user,
+            );
+            return view("app.index", $data);
+        } else {
+            return redirect("new");
+        }
+    }
 }
