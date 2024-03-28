0bdd36ea6df096c1e1d573c0e07e8a1aaf85ed83
bigsky1118z <bigsky1118z@gmail.com>
2024-03-28 13:55:30 +0900

    table

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -6,20 +6,38 @@
     </x-slot>
     <x-slot name="main">
         <section>
-            <h2>user</h2>
+            <h2>ユーザー情報</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>メールアドレス</th>
+                        <th>ユーザー名</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{{ $user->id }}</td>
+                        <td><input type="email" name="email" value="{{ $user->email }}"></td>
+                        <td><input type="text" name="user_name" value="{{ $user->user_name }}"></td>
+                    </tr>
+                </tbody>
+            </table>
             <table>
                 <tbody>
                     <tr>
-                        <th>id</th>
-                        <td>{{ $user->id }}</td>
+                        <th>氏名</th>
+                        <td>
+                            <span>氏</span><input type="text" class="user_name" name="last_name" value="">
+                            <span>名</span><input type="text" class="user_name" name="first_name" value="">
+                        </td>
                     </tr>
                     <tr>
-                        <th>email</th>
-                        <td><input type="email" name="email" value="{{ $user->email }}"></td>
-                    </tr>
-                    <tr>
-                        <th>user name</th>
-                        <td><input type="text" name="user_name" value="{{ $user->user_name }}"></td>
+                        <th>氏名</th>
+                        <td>
+                            <span>氏</span><input type="text" class="user_name" name="last_name_kana" value="">
+                            <span>名</span><input type="text" class="user_name" name="first_name_kana" value="">
+                        </td>
                     </tr>
                 </tbody>
             </table>
