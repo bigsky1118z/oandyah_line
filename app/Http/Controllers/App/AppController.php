34a68084aa3b066c044a34601f5298df02fad79d
bigsky1118z <bigsky1118z@gmail.com>
2024-03-29 13:51:25 +0900

    validated

--- app/Http/Controllers/App/AppController.php
+++ app/Http/Controllers/App/AppController.php
@@ -26,10 +26,10 @@ class AppController extends Controller
     public function store(Request $request)
     {
         $validated = $request->validate([
-            "app_name"              => ["required","unique:apps,app_name","min:4","max:16"],
-            "channel_access_token"  => ["required","unique:apps"],
-        ]);   
-        return $request->all();
+            "app_name"  => ["required","unique:apps,app_name","min:4","max:16"],
+            // "channel_access_token"  => ["required","unique:apps,channel_access_token"],
+        ]);
+        return $validated;
     }
 
 
