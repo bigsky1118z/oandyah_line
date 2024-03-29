73538d2082e91f841cb3b48a63308aee9520b1ab
bigsky1118z <bigsky1118z@gmail.com>
2024-03-29 14:12:58 +0900

    error

--- app/Http/Controllers/App/AppController.php
+++ app/Http/Controllers/App/AppController.php
@@ -36,7 +36,7 @@ class AppController extends Controller
         ]);
         $app_name               =   $request->get("app_name");
         $channel_access_token   =   $request->get("channel_access_token");
-        $channel_access_token   =   "46jMDeKXz36hFGeefYyNJ906lND6bcTmn3E9BXy2dO5qvj1BqUmsCKF79g44eFk+0LyRD75pNGCVWw3PkVm948DZMFEifDfld+fhFvta4eWCIxfEpaMj8dF4EdWk0aw66BWCFsVkpRJu8nrAhQKgaAdB04t89/1O/w1cDnyilFU=";
+        // $channel_access_token   =   "46jMDeKXz36hFGeefYyNJ906lND6bcTmn3E9BXy2dO5qvj1BqUmsCKF79g44eFk+0LyRD75pNGCVWw3PkVm948DZMFEifDfld+fhFvta4eWCIxfEpaMj8dF4EdWk0aw66BWCFsVkpRJu8nrAhQKgaAdB04t89/1O/w1cDnyilFU=";
         $response               =   App::get_bot_channel_webhook_endpoint($channel_access_token);
         if($response->successful()){
             return $response;
