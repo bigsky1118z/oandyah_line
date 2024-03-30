525709d0ab496202085092b95daf332c63ff4461
bigsky1118z <bigsky1118z@gmail.com>
2024-03-30 17:03:27 +0900

    1

--- resources/views/app/message/index.blade.php
+++ resources/views/app/message/index.blade.php
@@ -14,6 +14,7 @@
             <tbody>
                 @foreach ($app->messages as $message)
                     <tr>
+                        <td>{{ $message->message_object_1 }}</td>
                         <td>{{ $message }}</td>
                         <td><button type="button" onclick="location.href='/{{ $user->name }}/app/{{ $app->name }}/friend/{{ $message->id }}'">詳細</button></td>
                     </tr>                    
