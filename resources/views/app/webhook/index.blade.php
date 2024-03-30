f1eb235b5af76276d3dd8c01df79d43e88f4afd1
bigsky1118z <bigsky1118z@gmail.com>
2024-03-30 14:09:47 +0900

    app

--- resources/views/app/webhook/index.blade.php
+++ resources/views/app/webhook/index.blade.php
@@ -23,6 +23,7 @@
                         <td>{{ $webhook->type }}</td>
                         <td>{{ $webhook->response_status }}</td>
                         <td>{{ $webhook->mode }}</td>
+                        <td>{{ $webhook->app }}</td>
                         <td><button type="button" onclick="console.log('{{ $webhook->id }}')">詳細</button></td>
                     </tr>
                 @endforeach
