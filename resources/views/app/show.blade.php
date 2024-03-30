1c0dbd137af146a384e563aaa43d806df2017e14
bigsky1118z <bigsky1118z@gmail.com>
2024-03-30 10:14:01 +0900

    info

--- resources/views/app/show.blade.php
+++ resources/views/app/show.blade.php
@@ -7,10 +7,88 @@
     <x-slot name="main">
         <h2>{{ $app->diplay_name ?? $app->name }}</h2>
         <form action="/{{ $user->name }}/app/{{ $app->name }}" method="post">
+            @csrf
             <p>現在の権限 {{ $role }}</p>
-            <select name="role" id="">
-            </select>
-            {{-- @csrf --}}
+            <table>
+                <tr>
+                    <th>name</th>
+                    <td>
+                        @switch($role)
+                            @case("admin")
+                                <input type="text" name="name" value="{{ $app->name }}">
+                                @break
+                            @case("editor")
+                            @default
+                                {{ $app->name }}
+                                @break
+                        @endswitch
+                    </td>
+                </tr>
+                <tr>
+                    <th>channel_access_token</th>
+                    <td>
+                        @switch($role)
+                            @case("admin")
+                                <textarea name="channel_access_token" cols="50" rows="5">{{ $app->channel_access_token }}</textarea>
+                                @break
+                            @case("editor")
+                            @default
+                                {{ $app->channel_access_token }}
+                                @break
+                        @endswitch
+                    </td>
+                </tr>
+                <tr>
+                    <th>user_id</th>
+                    <td>{{ $app->user_id }}</td>
+                </tr>
+                <tr>
+                    <th>basic_id</th>
+                    <td>{{ $app->basic_id }}</td>
+                </tr>
+                <tr>
+                    <th>display_name</th>
+                    <td>
+                        @switch($role)
+                            @case("admin")
+                            @case("editor")
+                                <input type="text" name="display_name" value="{{ $app->display_name }}">
+                                @break
+                            @default
+                                {{ $app->display_name }}
+                                @break
+                        @endswitch
+                    </td>
+                </tr>
+                <tr>
+                    <th>picture_url</th>
+                    <td>
+                        <dl>
+                            <dt><img src="{{ $app->picture_url }}" alt=""></dt>
+                            @switch($role)
+                                @case("admin")
+                                @case("editor")
+                                    <dd>
+                                        <input type="text" name="picture_url" value="{{ $app->picture_url }}">
+                                        <button type="button"></button>
+                                    </dd>
+                                    @break
+                                @default
+                                    <dd></dd>
+                                    @break
+                            @endswitch
+                        </dl>
+                    </td>
+                </tr>
+                <tr>
+                    <th>chat_mode</th>
+                    <td>{{ $app->basic_id }}</td>
+                </tr>
+                <tr>
+                    <th>mark_as_read_mode</th>
+                    <td>{{ $app->basic_id }}</td>
+                </tr>
+            </table>
             <button type="submit">post</button>
         </form>
     </x-slot>
