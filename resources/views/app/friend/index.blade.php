a5f22d996829cd72c8e7e84a5c4069f962e91d6b
bigsky1118z <bigsky1118z@gmail.com>
2024-03-30 12:09:31 +0900

    app

--- resources/views/app/friend/index.blade.php
+++ resources/views/app/friend/index.blade.php
@@ -18,13 +18,6 @@
                 <tr>
                     <td>{{ $app }}</td>
                 </tr>
-                {{-- @foreach ($app->frineds as $friend)
-                    <tr>
-                        <td>{{ $friend->created_at }}</td>
-                        <td>{{ $friend->friend_id }}</td>
-                        <td><button type="button" onclick="console.log('{{ $friend->id }}')">詳細</button></td>
-                    </tr>
-                @endforeach --}}
             </tbody>
         </table>
     </x-slot>
