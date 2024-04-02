6d33f1ddc5408e08ee65d83e97bea6c6dd75c4a8
bigsky1118z <bigsky1118z@gmail.com>
2024-04-02 16:44:42 +0900

    message

--- resources/views/app/webhook/index.blade.php
+++ resources/views/app/webhook/index.blade.php
@@ -20,6 +20,8 @@
                     <tr>
                         <td>{{ $webhook->created_at }}</td>
                         <td>{{ $webhook->get_friend()->get_name() }}</td>
+                        <td>{{ $webhook->get_friend()->status_message }}</td>
+                        
                         <td>{{ $webhook->get_type() }}</td>
                         <td>{{ $webhook->get_reply_token() }}</td>
                         <td>{{ $webhook->get_event("mode") }}</td>
