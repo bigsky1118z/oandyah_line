d302a47cd355a763755e21a8aa9029f50cc95da6
bigsky1118z <bigsky1118z@gmail.com>
2024-04-02 16:48:00 +0900

    stasts

--- resources/views/app/webhook/index.blade.php
+++ resources/views/app/webhook/index.blade.php
@@ -21,6 +21,7 @@
                         <td>{{ $webhook->created_at }}</td>
                         <td>{{ $webhook->get_friend()->get_name() }}</td>
                         <td>{{ $webhook->get_friend()->status_message }}</td>
+                        <td>{{ $webhook->get_friend()->status }}</td>
                         
                         <td>{{ $webhook->get_type() }}</td>
                         <td>{{ $webhook->get_reply_token() }}</td>
