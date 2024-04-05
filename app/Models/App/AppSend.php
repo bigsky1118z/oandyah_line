729f22dbf84b00cc2065348ee69f9867063f7e9e
bigsky1118z <bigsky1118z@gmail.com>
2024-04-05 16:36:07 +0900

    mesage

--- app/Models/App/AppSend.php
+++ app/Models/App/AppSend.php
@@ -97,9 +97,9 @@ class AppSend extends Model
         $response               =   Http::withHeaders($headers)->post($endpoint, $data);
         $this->response_code    =   $response->status();
         if($response->successful()){
-            $this->sent_messages    =   $response->get("sentMessages") ?? null;
+            $this->sent_messages    =   $response["sentMessages"] ?? null;
         } else {
-            $this->error_message    =   $response->get("message");
+            $this->error_message    =   $response["message"];
         }
         $this->save();
         
