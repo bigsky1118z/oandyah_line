59926eb396cc4f1dac8deecdd0fc62f253ad1e24
bigsky1118z <bigsky1118z@gmail.com>
2024-04-16 15:06:56 +0900

    desc

--- app/Models/App.php
+++ app/Models/App.php
@@ -50,7 +50,7 @@ class App extends Model
 
     public function messages()
     {
-        return $this->hasMany(AppMessage::class)->orderBy("default")->orderBy("enable")->orderBy("id");
+        return $this->hasMany(AppMessage::class)->orderByDesc("default")->orderByDesc("enable")->orderBy("id");
     }
 
     public function sends()
