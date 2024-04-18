6c9f268b240c062b20e526a92e54f8ef9b5733c0
bigsky1118z <bigsky1118z@gmail.com>
2024-04-18 10:59:14 +0900

    class

--- resources/views/components/web/messages/template.blade.php
+++ resources/views/components/web/messages/template.blade.php
@@ -1,8 +1,8 @@
 @switch($template["type"] ?? null)
     @case("buttons")
         <dl class="dl-messages-template-buttons">
-            <dt class="dl-messages-template-buttons-title"><b>{{ $template["title"] ?? null }}</b></dt>
-            <dd class="dl-messages-template-buttons-text">{{ $template["text"] ?? null }}</dd>
+            <dt class="dt-messages-template-buttons-title"><b>{{ $template["title"] ?? null }}</b></dt>
+            <dd class="dd-messages-template-buttons-text">{{ $template["text"] ?? null }}</dd>
             <dd class="dd-messages-template-buttons-acitons">
                 <dl>
                     @foreach ($template["actions"] as $action)
