aab635618234060e5dfa6ebc7e3a96def0cf256e
bigsky1118z <bigsky1118z@gmail.com>
2024-04-18 13:46:15 +0900

    prip

--- app/Models/App/AppReplyCondition.php
+++ app/Models/App/AppReplyCondition.php
@@ -27,18 +27,24 @@ class AppReplyCondition extends Model
 
     static function find_message($app_id, $type, $text = null)
     {
-        $conditions =   AppReplyCondition::whereAppId($app_id)->whereType($type)->whereEnable(true)->get();;
+        $conditions =   AppReplyCondition::whereAppId($app_id)->whereType($type)->whereEnable(true)->get();
         $results    =   $conditions->filter(function($condition) use($text) {
             $keyword    =   $condition->condition["keyword"]    ?? null;
             $match      =   $condition->condition["match"]      ?? null;
             switch($match){
-                case"partial_match":
+                case "forward_match":
+                    return strpos($text, $keyword) === 0;
+                case "backward_match":
+                    $keyword_length = strlen($keyword);
+                    $text_length    = strlen($text);
+                    return $keyword_length <= $text_length ? substr($text, -$keyword_length) === $keyword : false;
+               case"partial_match":
                     return strpos($text, $keyword) !== false;
                 case"exact_match":
                 default:
                     return $keyword == $text;
             }
-        });
+        })->sortBy("priority");
         return $results;
     }
 }
