b68d557864d821de8554122b8ded84621d52528f
bigsky1118z <bigsky1118z@gmail.com>
2024-04-18 13:06:50 +0900

    json

--- app/Models/App/AppReplyCondition.php
+++ app/Models/App/AppReplyCondition.php
@@ -27,7 +27,7 @@ class AppReplyCondition extends Model
     static function get_keywords($app_id)
     {
         $conditions =   AppReplyCondition::whereAppId($app_id)->whereType("message")->whereEnable(true)->whereNotNull("condition->keyword")->get();
-        $keywords   =   $conditions->pluck("condition->keyword");
+        $keywords   =   $conditions->pluck("condition");
         return $keywords;
 
     }
