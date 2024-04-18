f3e3675f08ceb57f0a948d276dff33b857a4fdb3
bigsky1118z <bigsky1118z@gmail.com>
2024-04-18 10:50:11 +0900

    poinete

--- resources/views/components/web/messages.blade.php
+++ resources/views/components/web/messages.blade.php
@@ -2,10 +2,10 @@
     @foreach ($messages as $message)
         @switch($message["type"])
             @case("text")
-                <li><x-web.message_object.text :text="$message['text']" /></li>
+                <li><x-web.messages.text :text="$message['text']" /></li>
                 @break
             @case("template")                                            
-                <li><x-web.message_object.template :template="$message['template']" /></li>
+                <li><x-web.messages.template :template="$message['template']" /></li>
                 @break
             @default
                 @break
