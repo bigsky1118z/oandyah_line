b2fb5c2cb742b222c6d32ebe34c0c80de1380ec6
bigsky1118z <bigsky1118z@gmail.com>
2024-04-25 16:51:56 +0900

    inner

--- resources/views/app/reply/create.blade.php
+++ resources/views/app/reply/create.blade.php
@@ -9,6 +9,7 @@
         <h3>自動返信 新規作成</h3>
         <form action="/{{ $user->name }}/app/{{ $app->name }}/reply" method="post">
             @csrf
+            <p><button type="submit">保存</button></p>
             <p><input type="text" name="name" required></p>
             <table>
                 <thead>
@@ -20,8 +21,8 @@
                 <tbody>
                     @for ($i = 1; $i <= 5; $i++)
                         <tr>
-                            <td>
-                                <select name="messages[{{ $i }}][type]" onchange="confirm_change(this);" data-previous-value="{{ $i===1 ? "text" : "" }}" @required($i===1)>
+                            <td id="td-messages-{{ $i }}-type">
+                                <select name="messages[{{ $i }}][type]" onchange="change_message_type(this);" data-previous-value="{{ $i===1 ? "text" : "" }}" data-index="{{ $i }}" @required($i===1)>
                                     @if ($i !== 1)
                                         <option value="">---</option>
                                     @endif
@@ -29,33 +30,46 @@
                                     <option value="template">テンプレート</option>
                                 </select>
                             </td>
-                            <td>
+                            <td id="td-messages-{{ $i }}-message">
                                 @if ($i===1)
-                                    <textarea name="messages[{{ $i }}][text]" cols="50" rows="5"></textarea>
+                                    <div id="messages-{{ $i }}-text">
+                                        <textarea name="messages[{{ $i }}][text]" cols="50" rows="5"></textarea>
+                                    </div>
                                 @endif
                             </td>
                         </tr>
                     @endfor
                 </tbody>
             </table>
+            <p><button type="submit">保存</button></p>
         </form>
+    </x-slot>
+    <x-slot name="hidden">
+        <div id="messages-sumple-text">
+            <textarea name="messages[sumple][text]" cols="50" rows="5"></textarea>
+        </div>
+        <div id="messages-sumple-template">
+            <p>まだないよー</p>
+        </div>
     </x-slot>
     <x-slot name="footer">
     </x-slot>
-    <x-slot name="hidden">
-    </x-slot>
     <x-slot name="script">
         <script>
-            function confirm_change(select){
+            function change_message_type(select){
                 const value     =   select.value;
+                const index     =   select.getAttribute("data-index");
+                const previous  =   select.getAttribute("data-previous-value");
                 const result    =   window.confirm("タイプを変更すると対象のメッセージの内容が失われます。\nタイプを変更しますか？");
-                if(result || value ==""){
+                if(result || previous == ""){
                     select.setAttribute("data-previous-value", value);
-                    console.log("true", value);
+                    const div   =   document.getElementById(`messages-sumple-${value}`).clone();
+                    div.id      =   `messages-${index}-${value}`;
+                    document.getElementById(`td-messages-${id}-message`).innerHTML = div;
                 } else {
-                    select.value    =   select.getAttribute("data-previous-value");
-                    console.log("false", select.getAttribute("data-previous-value"));
+                    select.value    =   previous;
                 }
+                
             }
         </script>
     </x-slot>
