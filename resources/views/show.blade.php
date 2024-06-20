770bb67e4a697e544f5f76030f9f15c06d24f96a
bigsky1118z <bigsky1118z@gmail.com>
2024-06-20 17:22:18 +0900

    マイページ

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,11 +1,11 @@
 <x-frame.web>
     <x-slot name="id">user</x-slot>
-    <x-slot name="title">{{ $user->get_name() ?? null }}</x-slot>
+    <x-slot name="title">{{ $user->get_name() ?? null }}さんのマイページ</x-slot>
     <x-slot name="description"></x-slot>
     <x-slot name="head"></x-slot>
     <x-slot name="header"></x-slot>
     <x-slot name="main">
-        <h2>{{ $user->get_name() ?? null }} - TOP</h2>
+        <h2>{{ $user->get_name() ?? null }}さんのマイページ</h2>
         <h3><a href="{{ asset($user->name . '/app') }}">アプリ一覧</a></h3>
         <ul>
             @foreach ($user->apps as $app)
