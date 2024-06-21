ae7f1c830b136c3f4e9ad1334226f3c8e56cc55c
bigsky1118z <bigsky1118z@gmail.com>
2024-06-21 15:49:11 +0900

    backup

--- resources/views/admin/backup/show.blade.php
+++ resources/views/admin/backup/show.blade.php
@@ -7,13 +7,28 @@
     <x-slot name="page_transition_list">
         <li><a href="{{ asset('admin') }}">TOP</a></li>
         <li><a href="{{ asset('admin'.'/backup') }}">バックアップ</a></li>
-        <li><a href="{{ asset('admin'.'/backup/'.($table["table_name"] ?? null)) }}">{{ $table["title"] }}</a></li>
+        <li><a href="{{ asset('admin'.'/backup/'.($table["table_name"] ?? null)) }}">{{ $table["title"] }}復元</a></li>
     </x-slot>
     <x-slot name="main">
         <h2>バックアップ</h2>
         <section>
-            <h3>復元</h3>
-            
+            <form action="{{ asset('admin/backup/'.($table["table_name"] ?? null)) }}" method="post">
+                @csrf
+                <h3>{{ $table["title"] }}復元</h3>
+                <table>
+                    <tr>
+                        <th>バックアップデータ</th>
+                        <td>
+                            <select name="file_name">
+                                @foreach ($file_names as $file_name)
+                                    <option value="{{ $file_name }}">{{ str_replace("private/backup/".($table["table_name"] ?? ""),"",$file_name) }}</option>
+                                @endforeach
+                            </select>
+                        </td>
+                        <td><button type="submit">復元</button></td>
+                    </tr>
+                </table>
+            </form>
         </section>
     </x-slot>
     <x-slot name="footer"></x-slot>
